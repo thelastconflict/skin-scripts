6cc1cd5d5f163e917fb8a51f4060d8cb4a32eaaf
thelastconflict <95184992+thelastconflict@users.noreply.github.com>
2022-03-20 16:35:15 -0700

    initial

---
+++ TLC-COPY-PASTER-V1.lua
@@ -0,0 +1,263 @@
+--[[ 
+    NOTE!! ASEPRITE COUNTS LAYERS FROM BOTTOM TO TOP, BOTTOM MOST LAYER HAS INDEX OF 1
+TODO: option to spread all the blink heads to a layer of choice while automatically positioning them based off top left corner
+    - spread the one eyes to tiredidle, hurtwalk, hurtidle
+TODO: option to spread all the walk bodies to a layer of choice while automatically positioning them based off BOTTOM RIGHT CORNER?
+]] local sprite = app.activeSprite
+local layers = sprite.layers
+
+local dlg = Dialog()
+
+local layer_names = {}
+
+for i, layer in ipairs(layers) do
+    table.insert(layer_names, i, layer.name)
+    -- print(i, layer.name)
+end
+
+function get_layer_index(ls, name)
+    local res = nil
+    for i, layer in ipairs(ls) do
+        if layer.name == name then
+            res = i
+            break
+        end
+    end
+    if res == nil then print("WARNING! COULD NOT FIND LAYER" .. name) end
+    return res
+end
+
+function get_tag(ls, name)
+    local res = nil
+    for i, t in ipairs(ls) do if t.name == name then res = t end end
+    if res == nil then print("WARNING! COULD NOT FIND TAG" .. name) end
+    return res
+end
+
+-- list of tags, tagname -> list<cels>
+function get_cels_by_tag(ls, name, layer)
+    local res = {}
+    local tag = get_tag(ls, name)
+    local from_frame = tag.fromFrame.frameNumber
+    local to_frame = tag.toFrame.frameNumber
+    for index = from_frame, to_frame do
+        local cel = layer:cel(index)
+        table.insert(res, cel)
+    end
+    return res
+end
+
+-- layer, number, number, layer, list<cel>
+-- if 1 cel copy is provided, it will be spread accross all.
+function pastey(to_layer, tagname, existing_layer, copies, match_bottom)
+    local tag = get_tag(sprite.tags, tagname)
+    local from_frame = tag.fromFrame.frameNumber
+    local to_frame = tag.toFrame.frameNumber
+
+    local count = 1
+    for index = from_frame, to_frame do
+        local to_cel = existing_layer:cel(index)
+        local copy = copies[count]
+
+        local to_cel_bottom_y = to_cel.position.y + to_cel.bounds.height 
+        local copy_cel_bottom_y = copy.position.y + copy.bounds.height
+        
+        -- MATCH THE BOTTOM MOST PIXELS WITH EACH OTHER! SO THEY LINE UP
+        local y_offset = (to_cel_bottom_y - copy_cel_bottom_y)
+        local position = to_cel.position -- has to be to cel for the head to easily line up, match bottom is for
+        if match_bottom then 
+            position = copy.position
+            position.y = position.y + y_offset
+        end
+        --[[ print(tagname, to_cel.position, to_cel.image.height,
+              to_cel.bounds.height, y_offset) ]]
+        if to_cel ~= nil then
+            to_layer.sprite:newCel(to_layer, index, copy.image,
+                                   position)
+            count = count + 1
+            if count > #copies then
+                -- if only provided 1 copy, then spread that accross
+                count = 1
+            end
+        end
+    end
+end
+
+local HEAD_LAYER_INDEX = get_layer_index(layers, "head")
+local BODY_LAYER_INDEX = get_layer_index(layers, "body")
+
+local HEAD_PASTE_SELECT = "HEAD LAYER PASTE SELECT"
+local head_paste_index = #layers --get_layer_index(layers, "head")
+local HEAD_PASTE_CONFIRM = "HEAD_PASTE_CONFIRM"
+local BODY_PASTE_SELECT = "BODY LAYER PASTE SELECT"
+local body_paste_index = #layers -- get_layer_index(layers, "body")
+local BODY_PASTE_CONFIRM = "BODY_PASTE_CONFIRM"
+
+local default = layers[#layers].name -- the top most layer
+dlg:combobox{
+    id = HEAD_PASTE_SELECT,
+    label = HEAD_PASTE_SELECT,
+    option=default,
+    options = layer_names,
+    onchange = function()
+        local lname = dlg.data[HEAD_PASTE_SELECT]
+        head_paste_index = get_layer_index(layers, lname)
+    end
+}:button{
+    id = HEAD_PASTE_CONFIRM,
+    label = "",
+    text = "paste",
+    onclick = function()
+        local hlayer = layers[HEAD_LAYER_INDEX]
+        local eyes_full_cel_e = hlayer:cel(1)
+        local eyes_part_cel_e = hlayer:cel(2)
+        local eyes_closed_cel_e = hlayer:cel(3)
+
+        local eyes_full_cel_s = hlayer:cel(7)
+        local eyes_part_cel_s = hlayer:cel(8)
+        local eyes_closed_cel_s = hlayer:cel(9)
+
+        local eyes_full_cel_w = hlayer:cel(12)
+        local eyes_part_cel_w = hlayer:cel(13)
+        local eyes_closed_cel_w = hlayer:cel(14)
+
+        local eyes_closed_cel_n = hlayer:cel(6)
+
+        -- NEED TO PASTE FROM MATCHING BOTTOMS, NEED TO OFFSET Y
+        local to_layer = layers[head_paste_index]
+        pastey(to_layer, "hurtidle_e", hlayer,
+               {eyes_part_cel_e, eyes_closed_cel_e}, false)
+        pastey(to_layer, "hurtidle_s", hlayer,
+               {eyes_part_cel_s, eyes_closed_cel_s}, false)
+        pastey(to_layer, "hurtidle_w", hlayer,
+               {eyes_part_cel_w, eyes_closed_cel_w}, false)
+        pastey(to_layer, "hurtidle_n", hlayer, {eyes_closed_cel_n}, false)
+
+        pastey(to_layer, "hurtwalk_e", hlayer, {eyes_part_cel_e}, false)
+        pastey(to_layer, "hurtwalk_w", hlayer, {eyes_part_cel_w}, false)
+        pastey(to_layer, "hurtwalk_n", hlayer, {eyes_closed_cel_n}, false)
+        pastey(to_layer, "hurtwalk_s", hlayer, {eyes_part_cel_s}, false)
+
+        pastey(to_layer, "run_e", hlayer, {eyes_full_cel_e}, false)
+        pastey(to_layer, "run_w", hlayer, {eyes_full_cel_w}, false)
+        pastey(to_layer, "run_n", hlayer, {eyes_closed_cel_n}, false)
+        pastey(to_layer, "run_s", hlayer, {eyes_full_cel_s}, false)
+
+        pastey(to_layer, "sleep_e", hlayer, {eyes_closed_cel_e}, false)
+        pastey(to_layer, "sleep_w", hlayer, {eyes_closed_cel_w}, false)
+        pastey(to_layer, "sleep_n", hlayer, {eyes_closed_cel_n}, false)
+        pastey(to_layer, "sleep_s", hlayer, {eyes_closed_cel_s}, false)
+
+        -- tired idle does not blink, eyes lowered but body bobs
+        pastey(to_layer, "tiredidle_e", hlayer, {eyes_part_cel_e}, false)
+        pastey(to_layer, "tiredidle_s", hlayer, {eyes_part_cel_s}, false)
+        pastey(to_layer, "tiredidle_w", hlayer, {eyes_part_cel_w}, false)
+        pastey(to_layer, "tiredidle_n", hlayer, {eyes_closed_cel_n}, false)
+
+        pastey(to_layer, "walk_e", hlayer, {eyes_full_cel_e}, false)
+        pastey(to_layer, "walk_w", hlayer, {eyes_full_cel_w}, false)
+        pastey(to_layer, "walk_n", hlayer, {eyes_closed_cel_n}, false)
+        pastey(to_layer, "walk_s", hlayer, {eyes_full_cel_s}, false)
+
+        pastey(to_layer, "wink_e", hlayer, {eyes_closed_cel_e}, false)
+        pastey(to_layer, "wink_w", hlayer, {eyes_closed_cel_w}, false)
+        pastey(to_layer, "wink_n", hlayer, {eyes_closed_cel_n}, false)
+        pastey(to_layer, "wink_s", hlayer, {eyes_closed_cel_s}, false)
+
+        pastey(to_layer, "drawn_e", hlayer, {eyes_full_cel_e}, false)
+        pastey(to_layer, "drawn_w", hlayer, {eyes_full_cel_w}, false)
+        pastey(to_layer, "drawn_n", hlayer, {eyes_closed_cel_n}, false)
+        pastey(to_layer, "drawn_s", hlayer, {eyes_full_cel_s}, false)
+        -- WE DONT DO IDLE BECAUSE ITS AN EASY COPY PASTE OF BLINK AND WE CANNOT ASSUME THE LENGTH OF IDLE IS THAT OF BLINK
+        -- SKIPPING DRAWN ALSO
+    end
+}:combobox{
+    id = BODY_PASTE_SELECT,
+    label = BODY_PASTE_SELECT,
+    option=default,
+    options = layer_names,
+    onchange = function()
+        local lname = dlg.data[BODY_PASTE_SELECT]
+        body_paste_index = get_layer_index(layers, lname)
+    end
+}:button{
+    id = BODY_PASTE_CONFIRM,
+    label = "",
+    text = "paste",
+    onclick = function()
+        local blayer = layers[BODY_LAYER_INDEX]
+        local to_layer = layers[body_paste_index]
+        -- print(body_paste_index)
+        -- todo: paste bodies from walk
+
+        -- WE HAVE TO GET THE WALK FRAMES BECAUSE WE DONT KNOW WHERE THEY MIGHT BE DUE TO IDLE BEING ARBITRARILY LONG
+        -- CANNOT ASSUME THE 2 STILLS ARE THE SAME! BECAUSE SOME SKINS MIGHT HAVE SOMETHING ON THE CHARACTER THAT MOVES WITH THE BODY
+        local walk_e_cels = get_cels_by_tag(sprite.tags, "walk_e", blayer)
+        local leg_spread_e = walk_e_cels[1]
+        local leg_still_e1 = walk_e_cels[2]
+        local leg_close_e = walk_e_cels[3]
+        local leg_still_e2 = walk_e_cels[4]
+
+        pastey(to_layer, "run_e", blayer, walk_e_cels, true)
+
+        local walk_n_cels = get_cels_by_tag(sprite.tags, "walk_n", blayer)
+        local leg_spread_n = walk_n_cels[1]
+        local leg_still_n1 = walk_n_cels[2]
+        local leg_close_n = walk_n_cels[3]
+        local leg_still_n2 = walk_n_cels[4]
+
+        pastey(to_layer, "run_n", blayer, walk_n_cels, true)
+
+        local walk_s_cels = get_cels_by_tag(sprite.tags, "walk_s", blayer)
+        local leg_spread_s = walk_s_cels[1]
+        local leg_still_s1 = walk_s_cels[2]
+        local leg_close_s = walk_s_cels[3]
+        local leg_still_s2 = walk_s_cels[4]
+        pastey(to_layer, "run_s", blayer, walk_s_cels, true)
+
+        local walk_w_cels = get_cels_by_tag(sprite.tags, "walk_w", blayer)
+        local leg_spread_w = walk_w_cels[1]
+        local leg_still_w1 = walk_w_cels[2]
+        local leg_close_w = walk_w_cels[3]
+        local leg_still_w2 = walk_w_cels[4]
+
+        pastey(to_layer, "run_w", blayer, walk_w_cels, true)
+
+        pastey(to_layer, "blink_e", blayer, {leg_still_e1}, true)
+        pastey(to_layer, "blink_n", blayer, {leg_still_n1}, true)
+        pastey(to_layer, "blink_s", blayer, {leg_still_s1}, true)
+        pastey(to_layer, "blink_w", blayer, {leg_still_w1}, true)
+
+        pastey(to_layer, "hurtidle_e", blayer, {leg_still_e1}, true)
+        pastey(to_layer, "hurtidle_n", blayer, {leg_still_n1}, true)
+        pastey(to_layer, "hurtidle_s", blayer, {leg_still_s1}, true)
+        pastey(to_layer, "hurtidle_w", blayer, {leg_still_w1}, true)
+
+        pastey(to_layer, "sleep_e", blayer, {leg_still_e1}, true)
+        pastey(to_layer, "sleep_n", blayer, {leg_still_n1}, true)
+        pastey(to_layer, "sleep_s", blayer, {leg_still_s1}, true)
+        pastey(to_layer, "sleep_w", blayer, {leg_still_w1}, true)
+
+        pastey(to_layer, "wink_e", blayer, {leg_still_e1}, true)
+        pastey(to_layer, "wink_n", blayer, {leg_still_n1}, true)
+        pastey(to_layer, "wink_s", blayer, {leg_still_s1}, true)
+        pastey(to_layer, "wink_w", blayer, {leg_still_w1}, true)
+
+        pastey(to_layer, "tiredidle_e", blayer, {leg_still_e1}, true)
+        pastey(to_layer, "tiredidle_n", blayer, {leg_still_n1}, true)
+        pastey(to_layer, "tiredidle_s", blayer, {leg_still_s1}, true)
+        pastey(to_layer, "tiredidle_w", blayer, {leg_still_w1}, true)
+
+        pastey(to_layer, "hurtwalk_e", blayer, {leg_close_e, leg_still_e1}, true)
+        pastey(to_layer, "hurtwalk_n", blayer, {leg_close_n, leg_still_n1}, true)
+        pastey(to_layer, "hurtwalk_s", blayer, {leg_close_s, leg_still_s1}, true)
+        pastey(to_layer, "hurtwalk_w", blayer, {leg_close_w, leg_still_w1}, true)
+
+        pastey(to_layer, "drawn_e", blayer, {leg_spread_e, leg_close_e}, false)
+        pastey(to_layer, "drawn_n", blayer, {leg_spread_n, leg_close_n}, false)
+        pastey(to_layer, "drawn_s", blayer, {leg_spread_s, leg_close_s}, false)
+        pastey(to_layer, "drawn_w", blayer, {leg_spread_w, leg_close_w}, false)
+        -- blayer:cel(1)
+    end
+}:show()
+
---
+++ TLC-EXPORT-V1.lua
@@ -0,0 +1,228 @@
+--[[ TLC Export v1 
+
+This script scans animations and removes/combines CONSECUTIVE duplicates!
+
+]] -- NOTE: The head AND body need to be exactly the same pixels in order for it to be considered duplicate WITHIN the same tag context
+-- TODO: check average frame area for head and body and warn that there may be head pixels in the body
+local sprite = app.activeSprite
+local layers = sprite.layers
+
+local head_layer = layers[1]
+local body_layer = layers[2]
+
+local all_tag_names = {}
+
+-- idle animations are optional and will default to blink if not found
+local all_anim_names = {
+    "drawn_e", "drawn_n", "drawn_s", "drawn_w", "hurtwalk_e", "hurtwalk_n",
+    "hurtwalk_s", "hurtwalk_w", "idle_e", "idle_n", "idle_s", "idle_w", "run_e",
+    "run_n", "run_s", "run_w", "sleep_e", "sleep_n", "sleep_s", "sleep_w",
+    "walk_e", "walk_n", "walk_s", "walk_w", "hurtidle_e", "hurtidle_n",
+    "hurtidle_s", "hurtidle_w", "tiredidle_e", "tiredidle_n", "tiredidle_s",
+    "tiredidle_w", "melee_e", "shoothandgun_e", "shootmachineh_e",
+    "shootshotgun_e", "wink_e", "wink_s", "wink_w", "wink_n", "blink_e", "blink_s", "blink_w", "blink_n"
+}
+-- THE INDEXES ARE NOT ACCURATE AS THESE CAN BE SWAPPED, SO WE DOUBLE CHECK
+if head_layer.name ~= "head" then
+    local tmp = body_layer
+    body_layer = head_layer
+    head_layer = tmp
+end
+
+local everything_is_good = true 
+
+if #layers > 2 then 
+    everything_is_good = false
+    print("!!!CRITICAL!!!: YOU HAVE MORE THAN 2 LAYERS?!")
+end
+
+-- Stores (index, image)
+local unique_heads = {}
+local unique_bodies = {}
+
+local IMG = "img"
+local INDEX = "index"
+local NAME = "name"
+
+function add_unique(ls, compare, frame_index, anim_name)
+    local is_unique = true
+    for index, test in ipairs(ls) do
+        if compare:isEqual(test[IMG]) then
+            is_unique = false
+            break
+        end
+    end
+
+    if is_unique then
+        local new_entry = {}
+        new_entry[IMG] = compare
+        new_entry[INDEX] = frame_index
+        new_entry[NAME] = anim_name
+        ls[#ls + 1] = new_entry
+    end
+end
+
+for i, tag in ipairs(sprite.tags) do
+    local name = tag.name
+    all_tag_names[#all_tag_names + 1] = name
+    local frames = tag.frames
+    local start_index = tag.fromFrame.frameNumber
+    local end_index = tag.toFrame.frameNumber
+    local tsprite = tag.sprite
+
+    -- print(name, start_index, end_index)
+
+    for frame_index = start_index, end_index do
+        local head_cel = head_layer:cel(frame_index)
+        local body_cel = body_layer:cel(frame_index)
+
+        -- 20x15 for the head
+        -- 21x19 for the body
+        local warn_head_w = 20
+        local warn_head_h = 15
+
+        local warn_body_w = 21
+        local warn_body_h = 19
+
+        local next_index = frame_index + 1
+        if next_index > end_index then next_index = nil end
+
+        if head_cel ~= nil and body_cel ~= nil then
+            -- print(head_cel.bounds)
+            -- print(body_cel.bounds)
+            -- WARNS POTENTIALLY THAT THERE MIGHT BE MISPLACED PIXELS IN LAYERS, NOTE THIS IS SOMEWHAT BUGGED BECASUE OF ASEPRITE
+            -- https://github.com/aseprite/aseprite/issues/3206
+            -- FOR NOW JUST CHECK THE RESULTING OUTPUT TEXTURE
+            -- checks if less than 32 because of bugged bounds ^
+            local is_head_wide = head_cel.bounds.width > warn_head_w and head_cel.bounds.width < 32
+            local is_head_tall = head_cel.bounds.height > warn_head_h and head_cel.bounds.height < 32
+            local is_body_wide = body_cel.bounds.width > warn_body_w and body_cel.bounds.width < 32
+            local is_body_tall = body_cel.bounds.height > warn_body_h and body_cel.bounds.height < 32
+
+     --[[        print(" head " .. head_cel.bounds.width .. " h " ..
+                      head_cel.bounds.height)
+            print(" body " .. body_cel.bounds.width .. " h " ..
+                      body_cel.bounds.height) ]]
+            if is_head_wide or is_head_tall then
+                print(
+                    "!!!CHECK!!! : Are you sure there arent any misplaced pixels at LAYER " ..
+                        "HEAD '" .. name .. "' #" .. frame_index)
+            end
+            if is_body_wide or is_body_tall then
+                print(
+                    "!!!CHECK!!!: Are you sure there arent any misplaced pixels at LAYER " ..
+                        "BODY '" .. name .. "' #" .. frame_index)
+            end
+
+            local h_img = head_cel.image
+            local b_img = body_cel.image
+            if next_index ~= nil then
+                local next_h_cel = head_layer:cel(next_index)
+                local next_b_cel = body_layer:cel(next_index)
+                local next_h_img = next_h_cel.image
+                local next_b_img = next_b_cel.image
+
+                -- THIS SEEMS TO COMPARE THEM TRIMMED :D!
+                local is_head_eq = h_img:isEqual(next_h_img)
+                local is_body_eq = b_img:isEqual(next_b_img)
+
+                -- print(name, frame_index, next_index, "head ", is_head_eq, "body ", is_body_eq)
+                if is_head_eq and is_body_eq then
+                    everything_is_good = false
+                    print(
+                        "!!OPTIMIZE!! '" .. name .. "' " .. frame_index .. " to " ..
+                            next_index ..
+                            " are the same and can be merged into 1 frame with the combined duration!")
+                end
+                --[[ if is_body_eq == false then
+                    print(b_img.spec.colorMode, next_b_img.spec.colorMode)
+                    print(b_img.spec.width, next_b_img.spec.width)
+                    print(b_img.spec.height, next_b_img.spec.height)
+                    print(b_img.spec.transparentColor, next_b_img.spec.transparentColor)
+                end ]]
+            end
+
+            add_unique(unique_heads, h_img, frame_index, name)
+            add_unique(unique_bodies, b_img, frame_index, name)
+        end
+        -- print(tsprite)
+    end
+    -- print(name, start_index, end_index, tsprite, frames)
+end
+
+function print_uniques(ls, h_or_b)
+    for i, item in ipairs(ls) do
+        print(h_or_b .. " " .. item[NAME] .. " index " .. item[INDEX])
+    end
+end
+
+table.sort(all_anim_names)
+table.sort(all_tag_names)
+
+-- check to see if there are any missing animations
+for index, item in ipairs(all_anim_names) do
+    local is_missing = true
+    for _, j in ipairs(all_tag_names) do
+        if j == item then
+            is_missing = false
+            break
+        end
+    end
+    if is_missing then
+        everything_is_good = false
+        print("!!!CRITICAL WARNING!!!: The animation " .. item .. " IS MISSING")
+    end
+end
+
+if everything_is_good then 
+    print("WOOO! EVERYTHING IS GOOD :)")
+end
+
+print("===STATS===")
+
+print("==UNIQUIE HEADS==")
+print_uniques(unique_heads, "unique head")
+
+print("==UNIQUIE BODIES==")
+print_uniques(unique_bodies, "unique body")
+
+-- NOTE!: NOT ENTIRELY ACCURATE, CHECK OUTPUT TEXTURE, ITS PROBABLY AN ASEPRITE BUG?
+print("Total Unique heads: " .. #unique_heads .. " (average is less than 15)")
+print("Total Unique bodys: " .. #unique_bodies .. " (average is less than 60)")
+print("Combined " .. #unique_bodies + #unique_bodies)
+
+-- export, popup the file picker to prompt merging of conesutive frames and printing of debug?
+-- https://github.com/aseprite/api/blob/main/api/command/ExportSpriteSheet.md#exportspritesheet
+
+local filename = sprite.filename
+
+local name_without_ext = filename:sub(1, -5)
+
+app.command.ExportSpriteSheet {
+  ui=true,
+  askOverwrite=true,
+  type=SpriteSheetType.PACKED,
+  columns=0,
+  rows=0,
+  width=0,
+  height=0,
+  bestFit=true, -- no idea what this does
+  textureFilename=name_without_ext.. ".png",
+  dataFilename=name_without_ext.. ".json",
+  dataFormat=SpriteSheetDataFormat.JSON_ARRAY,
+  borderPadding=0,
+  shapePadding=2,
+  innerPadding=0,
+  trim=true,
+  trimSprite=true,
+  ignoreEmpty=true,
+  filenameFormat="{layer}-{title}_{tag}_{tagframe}_{duration}",
+  extrude=false,
+  openGenerated=false,
+  layer="",
+  tag="",
+  splitLayers=true,
+  listLayers=true,
+  listTags=true,
+  listSlices=true
+}
